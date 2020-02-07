38beacd22148424a0f02fe8bf1aa7c77c5b93967
nrukt00 <nrukt00@gmail.com>
2020-02-07 10:20:56 +0000

    Updated BEARmod to v05

--- bearmod_fx.R
+++ bearmod_fx.R
@@ -1,9 +1,22 @@
-##### BEARmod v.0.3
+##### BEARmod v.0.5
 #
 # Basic Epidemic, Activity, and Response simulation model
+# 
+# v0.5 updates:
+# - Added functionality to input relative movement table
+# - added functionality for time-variable recovery rates
+#
+# v.0.4 Updates: 
+# - Model calibrated using HKU studies. 
+# - Fixed transmission term to Poisson distribution 
+# - Added "date" versatility to use non-contiguous dates
 #
 # This model runs a basic SEIR model, with stochastic exposure, incubation, recovery, and movement
-# Disease spread occurs each day, based on the movement patterns from specific days from Baidu data
+# Disease spread occurs each day, based on the movement patterns from specific days from Baidu data.
+#
+#
+#
+# See run_model_example.R for fully working example.
 #
 # TO DO:
 # - Add in infectious period for part of the exposed period (new category: exposed and infectious)
@@ -11,7 +24,6 @@
 #
 #
 #
-# - Should results (# people infected, etc) be integers?
 ######
 
 library(lubridate)
@@ -31,12 +43,15 @@ InitiatePop = function(pat_locator,initialInf,initialExp){
     nRecoveredToday = rep(0,NPat),
     nInfectedToday = rep(0,NPat),
     nExposedToday = rep(0,NPat),
-    nInfMovedToday = rep(0,NPat)
+    nInfMovedToday = rep(0,NPat),
+    controlled = rep(0,NPat)
   )
 }
 
 ##### Epidemic functions: exposure, infectivity, recovery  ####
-recoveryTimeStep = function(HPop, recrate){
+recoveryTimeStep = function(HPop, recrate_values,current_day){
+  recrate = subset(recrate_values,date == current_day)$recrate
+  print(paste0("Day ",current_day, " recovery rate: ", recrate))
   for (i in 1:length(HPop$nInf)){
   HPop$nRecoveredToday[i]= sum(rbinom(HPop$nInf[i],1,recrate))
   HPop$nInf[i] = HPop$nInf[i] - HPop$nRecoveredToday[i]
@@ -59,7 +74,7 @@ exposedtoinfTimeStep = function(HPop, exp_to_infrate){
 
 exposedTimeStep = function(HPop, exposerate){
   for (i in 1:length(HPop$nInf)){
-    HPop$nExposedToday[i]= round(sum(rbinom(HPop$nInf[i],1,exposerate)) * (1 - ( (HPop$nInf[i] + HPop$nExp[i]) / HPop$nTotal[i])))
+    HPop$nExposedToday[i]= sum(rpois(HPop$nInf[i],exposerate)) * (1 - ( (HPop$nInf[i] + HPop$nExp[i]) / HPop$nTotal[i]))
   if ( HPop$nExp[i] + HPop$nExposedToday[i] < HPop$nTotal[i] - HPop$nInf[i]) {
   HPop$nExp[i] = HPop$nExp[i] + HPop$nExposedToday[i]
   
@@ -76,7 +91,7 @@ exposedTimeStep = function(HPop, exposerate){
 
 
 ####### Activity functions: Human movement ####
-movementTimeStep = function(HPop, mobmat,day,control_df,relative_movement){
+movementTimeStep = function(HPop, mobmat,day,control_df){
   movement_matrix = matrix(0,NPat,NPat,dimnames=list(patIDs,patIDs))
   daily_move = subset(mobmat,Date == day)
   daily_move_mat = as.matrix(daily_move[,c(4:8)])
@@ -84,8 +99,20 @@ movementTimeStep = function(HPop, mobmat,day,control_df,relative_movement){
   for (i in 1:length(patIDs)){
     movement_matrix[i,i] = 1 - sum(movement_matrix[i,-i])
   }
-  if (length(which(control_df$control_date < day)) > 0){
-    movement_matrix = stopMovement(control_df,movement_matrix,day,relative_movement)
+  
+  HPop$controlled = 0
+  if (length(which(control_df$date == day)) > 0){
+    control_df_sub = subset(control_df,date == day)
+    for (i in 1:dim(control_df_sub)[1]){
+      HPop$controlled[which(HPop$names == control_df_sub$from[i])] = control_df_sub$relative_move[i]
+      if (control_df_sub$relative_move[i] < 1){
+    #print(paste0("Instating travel ban in patch ", which(HPop$names == control_df_sub$from[i]) , "; relative movement ",control_df_sub$relative_move[i], " on day ", day  ))
+      }
+    }
+  }
+  
+  if (sum(HPop$controlled)>0){
+    movement_matrix = stopMovement(HPop,movement_matrix,day)
   }
   
   
@@ -114,16 +141,18 @@ movementTimeStep = function(HPop, mobmat,day,control_df,relative_movement){
 
 ###### Response functions: Control
 #relative_movement is the proportion of original movement out/in that we want to keep -- ie. .1 = 10% of original movement rate
-stopMovement = function(control_df,mobmat,current_date,relative_movement){
-  stopping = which(control_df$control_date < current_date)
+stopMovement = function(HPop,mobmat,current_date){
+  stopping = which(HPop$controlled > 0)
     if (length(stopping) > 0){
     #  print(paste("stopping movement in patches", control_df$patIDs[stopping]))
-    control_patches = control_df$patIDs[stopping]
-    mobmat[control_patches,] = mobmat[control_patches,] * relative_movement
-    mobmat[,control_patches] = mobmat[,control_patches] * relative_movement
+      for (ctrl_pat in stopping){
+    control_patches = HPop$patIDs[ctrl_pat]
+    mobmat[control_patches,] = mobmat[control_patches,] * HPop$controlled[ctrl_pat]
+    mobmat[,control_patches] = mobmat[,control_patches] * HPop$controlled[ctrl_pat]
     for (i in 1:length(patIDs)){
       mobmat[i,i] = 1 - sum(mobmat[i,-i])
     }
+      }
     }
     mobmat
 }
@@ -132,30 +161,32 @@ stopMovement = function(control_df,mobmat,current_date,relative_movement){
 
 
 ###### Master function  ####
-runSim = function(HPop,pat_info,mobmat,day_start,day_end,recrate,exposerate,exposepd,relative_movement = 1) {
+runSim = function(HPop,pat_info,control_info,mobmat,day_list,recrate_values,exposerate,exposepd) {
   
   
   epidemic_curve <- data.frame(Date=as.Date(character()),
                                inf=c(),
                                stringsAsFactors=FALSE) 
   
-  all_dates = seq(day_start,day_end,by="days")
-  
-  all_spread = matrix(0,length(HPop$nInf),length(all_dates))
+
+  all_spread = matrix(0,length(day_list),length(HPop$nInf))
+  colnames(all_spread) = HPop$names
   #print(all_dates)
-  for (current_day in 1:length(all_dates)){
+  for (current_day in 1:length(day_list)){
     
-    #print(all_dates[current_day])
-    HPop = recoveryTimeStep(HPop,recrate)
+    #print(day_list[current_day])
+    HPop = recoveryTimeStep(HPop,recrate_values,day_list[current_day])
     HPop = exposedtoinfTimeStep(HPop,1/exposepd)
     HPop = exposedTimeStep(HPop,exposerate)
-    HPop = movementTimeStep(HPop,mobmat,all_dates[current_day],pat_info,relative_movement)
+    HPop = movementTimeStep(HPop,mobmat,day_list[current_day],control_info)
     #save(HPop,file=paste(current_day,".RData"))
-    epidemic_curve = rbind(epidemic_curve,data.frame(Date = all_dates[current_day], inf = sum(HPop$nInf)))
-    all_spread[,current_day] = HPop$nInf
+    epidemic_curve = rbind(epidemic_curve,data.frame(Date = day_list[current_day], inf = sum(HPop$nInf)))
+    all_spread[current_day,] = HPop$nInf
     
   }
-  list(HPop = HPop,epidemic_curve = epidemic_curve,all_spread=all_spread)
+  all_spread_2 = data.frame(dates = day_list,runday = 1:length(day_list))
+  all_spread_2= cbind(all_spread_2,all_spread)
+  list(HPop = HPop,epidemic_curve = epidemic_curve,all_spread=all_spread_2)
 }
 
 
